55033903d78398453618cc0a4f72bf2402e33db8
nialldillane <niall.dillane@gmail.com>
2020-01-21 11:10:46 +0000

    formatting

--- code/data/test/webscrape.py
+++ code/data/test/webscrape.py
@@ -8,15 +8,17 @@ uage = 'mac:TestWebScrape (by /u/Mus7ache)'
 reddit = praw.Reddit(client_id = cid, client_secret = csec, user_agent = uage)
 
 posts = list()
-for submission in reddit.subreddit('writingprompts').hot(limit=3):
+for submission in reddit.subreddit('writingprompts').hot(limit=4):
     posts.append(submission)
 
 for post in posts:
     splitTitle = post.title.split(']', 1)
     if splitTitle[0] == '[WP':
         print(splitTitle[1].lstrip())
+        print('~~~~~~~~~~~~~~~~~~')
         for top_level_comment in post.comments:
             if isinstance(top_level_comment, MoreComments):
                 continue
             if top_level_comment.author != 'AutoModerator':
                 print(top_level_comment.body)
+                print('_______________________\n')
