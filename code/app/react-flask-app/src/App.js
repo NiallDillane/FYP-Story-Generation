75ad7d71914dc74d6d44373ed07b1c2bc458d3e0
nialldillane <niall.dillane@gmail.com>
2020-03-25 11:16:31 +0000

    global state

--- code/app/react-flask-app/src/App.js
+++ code/app/react-flask-app/src/App.js
@@ -1,56 +1,69 @@
 import React, { useState, useEffect } from 'react';
+import { createGlobalState } from 'react-hooks-global-state';
 import clippy from './images/coolClippy.png';
 import playButton from './images/play-button.png'
 import pauseButton from './images/pause-button.png'
 import './App.css';
 
+
+const initialState = { story: "Once upon a time in Hollywoo" , play: false};
+const { useGlobalState } = createGlobalState(initialState);
+
+
 function GenStory() {
-	const [story, setStory] = useState(StoryContent());
+	const [story, setStory] = useGlobalState('story');
 
-	// useEffect(() => {
-	// 	fetch('/getText', {
-	// 		method:"POST",
-	// 		cache: "no-cache",
-	// 		headers:{
-	// 			"content_type":"application/json",
-	// 		},
-	// 		body:JSON.stringify(story)
-	// 		}
-	// 	)
-	// 	.then(res => res.json())
-	// 	.then(data => {
-	// 		console.log(data)
-	// 		setStory(data.text.slice(0, -1));
-	// 	});
-	// }, []);
+	useEffect(() => {
+		fetch('/getText', {
+			method:"POST",
+			cache: "no-cache",
+			headers:{
+				"content_type":"application/json",
+			},
+			body:JSON.stringify(story)
+			}
+		)
+		.then(res => res.json())
+		.then(data => {
+			console.log(data)
+			setStory(data.text.slice(0, -1));
+		});
+	}, []);
 
-	return (
-		<div name="text"
-			contentEditable="true"
-			className="text-body"
-			defaultValue={story}>
-		</div>
-	);
+	return null;
+
+	// return (
+	// 	<div name="text"
+	// 		contentEditable="true"
+	// 		className="text-body"
+	// 		defaultValue={story}>
+	// 	</div>
+	// );
 }
 
 function StoryPane() {
+	const [story, setStory] = useGlobalState('story');	
+	const [play, setPlay] = useGlobalState('play');
+
 	return(
 		<div name="text"
+			ref={r=>this.ref = r}
 			contentEditable="true"
-			className="text-body">
-			<StoryContent />
+			className="text-body"
+			onChange={() => setStory(this.ref.innerHTML)}>
+			{story}
+			{play ? <GenStory /> : ""}
 		</div>
 	)
 }
 
-function StoryContent() {
-	return "Once upon a time ";
-}
+// function StoryContent() {
+// 	return "Once upon a time ";
+// }
 
 function PlayButton(){
-	const [play, setPlay] = useState(false);
-	while (play) {
-		console.log({play});
+	const [play, setPlay] = useGlobalState('play');
+	if (play) {
 		return (
 			<div name="pauseButton"	className="playPause">
 				<img src={pauseButton} onClick={() => setPlay(false)} />
